0a62faa8878e1a2e96d2bc079b0901aa49247930
Merijn Laks <merijn.laks@student.uva.nl>
2023-01-31 20:21:37 +0100

    import and setup firebase dependency

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,21 @@ import { Router, Route, Routes } from '@solidjs/router';
 // link to other pages using the 'A' component of the router
 
 import { lazy } from 'solid-js';
-const Order = lazy(() => import('./components/Order'));
 
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAHL3a8NgIR31S97JHsAu2fG_1EMy2jb7o",
+    authDomain: "test-app-dd032.firebaseapp.com",
+    projectId: "test-app-dd032",
+    storageBucket: "test-app-dd032.appspot.com",
+    messagingSenderId: "229925355235",
+    appId: "1:229925355235:web:4fe2a98d0769b1a034cfd6"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const Order = lazy(() => import('./components/Order'));
 
 
 render(
