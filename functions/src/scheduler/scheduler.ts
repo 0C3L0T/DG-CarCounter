51f7f1eff86b661805bf8cef8f129880fdc035d2
Merijn Laks <merijn.laks@student.uva.nl>
2023-02-21 00:02:58 +0100

    fixed bug in transaction logic

--- functions/src/scheduler/scheduler.ts
+++ functions/src/scheduler/scheduler.ts
@@ -15,114 +15,100 @@ import * as admin from "firebase-admin";
 *       -order_id, string
 * */
 
+class Schedule {
+    slots: string[];
+    slots_available: number;
 
-// create a new document for a schedule day
-async function createScheduleDay(batch: admin.firestore.WriteBatch, date: Date) {
-    console.log("creating schedule day...");
-    const slots = 10;
-
-    await admin.firestore().collection("schedule").doc(date.toDateString()).set({
-        slots_available: slots,
-        slots: Array(slots).fill({order_id: ""})
-    });
-}
-
-
-async function getNextAvailableDay(batch: admin.firestore.WriteBatch): Promise<Date> {
-    console.log("getting next available day...");
-    // get the current day
-    const today = new Date();
-
-    // get the next day
-    const nextDay = new Date(today);
-    nextDay.setDate(today.getDate() + 1);
-
-    // get the schedule for the next day
-    const scheduleRef = admin.firestore().collection("schedule").doc(nextDay.toDateString())
-    const data = await scheduleRef.get()
-    if (data.exists) {
-        const snapshot = data.data()
-        if (snapshot) {
-            const slotsAvailable = snapshot.slots_available;
-            if (slotsAvailable > 0) {
-                return nextDay;
-            }
-        }
+    constructor(slots: string[], slots_available: number = 10) {
+        this.slots = slots;
+        this.slots_available = slots_available;
     }
 
-    // if there is no schedule for the next day, create one
-    await createScheduleDay(batch, nextDay);
-
-    return nextDay;
+    addOrderToSlot = (slot: number, order_id: string) => {
+        this.slots[slot] = order_id;
+        this.slots_available -= 1;
+    }
 }
 
-
-async function getNextAvailableSlot(date: Date): Promise<number> {
-    console.log("getting next available slot...");
-    // get the schedule for the day
-    const scheduleRef = admin.firestore().collection("schedule").doc(date.toDateString());
-    if (scheduleRef) {
-        console.log("we have a schedule ref");
-        const data = await scheduleRef.get();
-        const snapshot = data.data();
-        if (snapshot) {
-            console.log("we have a snapshot");
-            const slotsAvailable = snapshot.slots_available;
-            const slots = snapshot.slots;
-
-            // if there are slots available, return the first available slot
-            if (slotsAvailable > 0) {
-                for (let i = 0; i < slots.length; i++) {
-                    if (slots[i].order_id === "") {
-                        return i;
-                    }
+async function getNextAvailableSlot(schedules: { [date: string]: Schedule }): Promise<[Date, number]> {
+    // get the first schedule with slots available
+    for (const date in schedules) {
+        const schedule = schedules[date];
+        if (schedule.slots_available > 0) {
+            // get the first slot that is empty
+            for (let i = 0; i < schedule.slots.length; i++) {
+                if (schedule.slots[i] === "") {
+                    return [new Date(date), i];
                 }
             }
         }
     }
 
-    throw new Error("Error getting next available slot.");
+    // if no slots are available, we can create a new schedule for the next day
+    const lastScheduleDate = new Date(Object.keys(schedules)[Object.keys(schedules).length - 1]);
+    const nextScheduleDate = new Date(lastScheduleDate);
+    nextScheduleDate.setDate(nextScheduleDate.getDate() + 1);
+
+    return [nextScheduleDate, 0];
 }
 
-
-async function addOrderToSlot(batch: admin.firestore.WriteBatch, date: Date, slot: number, order_id: string) {
-    console.log("adding order to slot...");
-    const scheduleRef = admin.firestore().collection("schedule").doc(date.toDateString());
-    if (scheduleRef) {
-        const data = await scheduleRef.get()
-        const snapshot = data.data()
-        if (snapshot) {
-            const slotsAvailable = snapshot.slots_available;
-            const slots = snapshot.slots;
-
-            // add the order to the slot
-            slots[slot].order_id = order_id;
-
-            // update the slots_available
-            const newSlotsAvailable = slotsAvailable - 1;
-
-            await scheduleRef.update({
-                slots_available: newSlotsAvailable,
-                slots: slots,
-            });
-        }
-    }
-}
-
-
-export default async function scheduleOrder(batch: admin.firestore.WriteBatch, orderId: string, duration: number) {
+export default async function scheduleOrder(transaction: admin.firestore.Transaction, orderId: string, duration: number) {
     console.log("scheduling order...");
+
+    const duration_copy = duration;
+
+    const orderRef = admin.firestore().collection("orders").doc(orderId);
+    const order = await transaction.get(orderRef);
+
+    if (!order.exists) {
+        throw new Error("Order does not exist.");
+    }
+
+    const schedules: { [date: string]: Schedule } = {};
+    // fill schedule object with all the dates and slots from transaction
+    // get the last schedule with slots available
+    const scheduleRef = admin.firestore().collection("schedule");
+    const scheduleQuery = await scheduleRef.limit(1).get();
+    if (!scheduleQuery.empty) {
+        const lastSchedule = scheduleQuery.docs[0];
+        console.log("last schedule: ", lastSchedule.data());
+        schedules[lastSchedule.id] = new Schedule(lastSchedule.data().slots, lastSchedule.data().slots_available);
+    } else {
+        // if there are no schedules, create a new one
+        schedules[new Date().toDateString()] = new Schedule(Array(10).fill("") as []);
+    }
+
+    // now we can use the schedules object to add new orders to the schedule
+
     while (duration > 0) {
-        // get the next available day
-        const nextDay = await getNextAvailableDay(batch);
-
         // get the next available slot
-        const nextSlot = await getNextAvailableSlot(nextDay);
+        const [date, slot] = await getNextAvailableSlot(schedules);
 
-        // add the order to the slot
-        await addOrderToSlot(batch, nextDay, nextSlot, orderId);
+        // add the order to the slot and store in intermediate object
+        if (schedules[date.toDateString()]) {
+            // add order to existing schedule
+            schedules[date.toDateString()].addOrderToSlot(slot, orderId);
+        } else {
+            // create new schedule
+            const slots: [] = Array(10).fill("") as [];
+            schedules[date.toDateString()] = new Schedule(slots, 10);
+            schedules[date.toDateString()].addOrderToSlot(slot, orderId);
+        }
 
-        // decrement the duration
         duration--;
     }
+
+    // writing part
+    // iterate over intermediate object and write to db
+    for (const date in schedules) {
+        const schedule = schedules[date];
+        const scheduleRef = admin.firestore().collection("schedule").doc(date);
+        transaction.set(scheduleRef, {
+            slots: schedule.slots,
+            slots_available: schedule.slots_available,
+        });
+    }
+
+    // update the order status
+    transaction.update(orderRef, {status: "scheduled", duration: duration_copy});
 }
