f36894a1b32949ff2b612b5d46277d33c454f60d
Merijn Laks <merijn.laks@student.uva.nl>
2023-02-23 22:27:15 +0100

    moved html elements

--- src/components/overview/AllOrders.tsx
+++ src/components/overview/AllOrders.tsx
@@ -1,56 +1,59 @@
 import type { Component } from 'solid-js';
 import {createSignal, Show} from 'solid-js';
-import { getFirestore, collection, query, onSnapshot} from "firebase/firestore";
+import { getFirestore, collection, onSnapshot} from "firebase/firestore";
 import type { DocumentData } from "firebase/firestore";
 
 const db = getFirestore();
 
-const [orders, setOrders] = createSignal<DocumentData[]>([]);
-const orderQuery = query(collection(db, "orders"));
-onSnapshot(orderQuery, (querySnapshot) => {
-    const orders: DocumentData[] = [];
-    querySnapshot.forEach((doc) => {
-        orders.push(doc.data());
-    });
-    setOrders(orders);
-});
 
 const AllOrders: Component = () => {
+    const [orders, setOrders] = createSignal<DocumentData[]>([]);
+    const orderReference = collection(db, "orders");
+
+    onSnapshot(orderReference, (orderSnapshot) => {
+        const orders: DocumentData[] = [];
+        orderSnapshot.forEach((doc) => {
+            orders.push(doc.data());
+        });
+        setOrders(orders);
+    });
+
+
     return (
-        <Show
-            when={orders().length != 0}
-            fallback={<div>Er zijn nog geen orders</div>}
-         keyed>
         <div>
             <h1>Alle Orders</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Merk</th>
-                        <th>Type</th>
-                        <th>Carroserie</th>
-                        <th>Kleur</th>
-                        <th>Kenteken</th>
-                        <th>Behandeling</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {orders().map((order, __created_at) => (
+            <Show
+                when={orders().length != 0}
+                fallback={<div>Er zijn nog geen orders</div>}
+                keyed>
+                <table>
+                    <thead>
                         <tr>
-                            <td>{order.brand}</td>
-                            <td>{order.model}</td>
-                            <td>{order.bodyType}</td>
-                            <td>{order.color}</td>
-                            <td>{order.licensePlate}</td>
-                            <td>{order.plan}</td>
-                            <td>{order.status}</td>
+                            <th>Merk</th>
+                            <th>Type</th>
+                            <th>Carroserie</th>
+                            <th>Kleur</th>
+                            <th>Kenteken</th>
+                            <th>Behandeling</th>
+                            <th>Status</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+                    </thead>
+                    <tbody>
+                        {orders().map((order, __created_at) => (
+                            <tr>
+                                <td>{order.brand}</td>
+                                <td>{order.model}</td>
+                                <td>{order.bodyType}</td>
+                                <td>{order.color}</td>
+                                <td>{order.licensePlate}</td>
+                                <td>{order.plan}</td>
+                                <td>{order.status}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
         </Show>
+    </div>
     )
 }
 
