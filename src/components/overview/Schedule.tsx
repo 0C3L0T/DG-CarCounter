30bf1abd7361a2986b0ba33f3304bb05e605a297
Merijn Laks <merijn.laks@student.uva.nl>
2023-02-23 22:28:03 +0100

    schedule component finally works now

--- src/components/overview/Schedule.tsx
+++ src/components/overview/Schedule.tsx
@@ -1,69 +1,70 @@
-import type { Component } from 'solid-js';
-import {createSignal, For, Show} from 'solid-js';
-import { getFirestore, getDoc, doc, onSnapshot} from "firebase/firestore";
-import type { DocumentData } from "firebase/firestore";
+import type {Component} from 'solid-js';
+import {createResource, createSignal, Show} from 'solid-js';
+import type {DocumentData} from "firebase/firestore";
+import {doc, getDoc, getFirestore, onSnapshot} from "firebase/firestore";
 
 const db = getFirestore()
 
-const today = new Date();
-today.setDate(today.getDate() + 1)
-
-const scheduleRef = doc(db, "schedule", today.toDateString());
-const [orders, setOrders] = createSignal<DocumentData[]>([])
-onSnapshot(scheduleRef, (querySnapshot) => {
-    const _orders: DocumentData[] = [];
-    // get the slots array from the document
-    const data = querySnapshot.data();
-    if (data) {
-        data.slots.forEach((orderId: DocumentData) => {
-            //filter out the empty slots
-            if (orderId.order_id != "") {
-                // get the corresponding order from the slot
-                const orderDoc = doc(db, "orders", orderId.order_id);
-                getDoc(orderDoc).then((doc) => {
-                    if (doc.exists()) {
-                        _orders.push(doc.data());
-                    }
-                });
-            }
-        });
-    }
-    setOrders(_orders);
-    console.log(orders()) // works here
-});
 
 const Schedule: Component<{date: Date}> = (props) => {
-    // get the schedule for the given date
+    const scheduleRef = doc(db, "schedule", props.date.toDateString());
+
+    const [orderIds, setOrderIds] = createSignal<string[]>([]);
+    onSnapshot(scheduleRef,  (scheduleSnapshot) => {
+        const data = scheduleSnapshot.data();
+        const _orderIds: string[] = [];
+        data?.slots?.map((slot: DocumentData) => {
+            if (slot.toString() != "") {
+                _orderIds.push(slot.toString());
+            }
+        });
+        setOrderIds(_orderIds);
+    });
+
+    const [orders] = createResource(orderIds,async (orderIds) => {
+        const orders = orderIds.map(
+            async (orderId) => {
+                const order = await getDoc(doc(db, "orders", orderId));
+                if (order.exists()) {
+                    return order.data();
+                }
+            }
+        )
+        return Promise.all(orders);
+    });
+
     return (
         <div>
-            <h1>Schema voor {props.date.toLocaleDateString("nl-NL")}</h1>
-            <Show when={(orders().length != 0)} keyed={true} fallback={<div>Er zijn nog geen orders</div>}>
+            <h1>Orders voor {props.date.toLocaleDateString("nl-NL")}</h1>
+            <Show
+                when={(!orders.loading && orders()!.length != 0)}
+                fallback={<div>Er zijn nog geen orders</div>}
+                keyed>
                 <table>
                     <thead>
-                        <tr>
-                            <th>Merk</th>
-                            <th>Type</th>
-                            <th>Carroserie</th>
-                            <th>Kleur</th>
-                            <th>Kenteken</th>
-                            <th>Behandeling</th>
-                            <th>Status</th>
-                        </tr>
+                    <tr>
+                        <th>Merk</th>
+                        <th>Type</th>
+                        <th>Carroserie</th>
+                        <th>Kleur</th>
+                        <th>Kenteken</th>
+                        <th>Behandeling</th>
+                        <th>Status</th>
+                    </tr>
                     </thead>
                     <tbody>
-                        <For each={orders()}>
-                            {(order) => (
-                                <tr>
-                                    <td>{order.brand}</td>
-                                    <td>{order.model}</td>
-                                    <td>{order.bodyType}</td>
-                                    <td>{order.color}</td>
-                                    <td>{order.licensePlate}</td>
-                                    <td>{order.plan}</td>
-                                    <td>{order.status}</td>
-                                </tr>
-                            )}
-                        </For>
+                    {orders()!.map((order, __created_at) => (
+                        <tr>
+                            <td>{order!.brand}</td>
+                            <td>{order!.model}</td>
+                            <td>{order!.bodyType}</td>
+                            <td>{order!.color}</td>
+                            <td>{order!.licensePlate}</td>
+                            <td>{order!.plan}</td>
+                            <td>{order!.status}</td>
+                        </tr>
+                    ))}
+
                     </tbody>
                 </table>
             </Show>
