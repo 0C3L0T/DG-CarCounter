320665082e58a00cf40f84a1ba80189cfe229ffe
Merijn Laks <merijn.laks@student.uva.nl>
2023-03-14 14:43:41 +0100

    switched over to cloud dev server from local emulator

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,9 @@ import { Router, Route, Routes } from '@solidjs/router';
 import { lazy } from 'solid-js';
 
 import { initializeApp } from 'firebase/app';
-import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
-import {getFunctions, connectFunctionsEmulator} from "firebase/functions";
-import {getAuth, connectAuthEmulator} from "firebase/auth";
+// import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
+// import {getFunctions, connectFunctionsEmulator} from "firebase/functions";
+// import {getAuth, connectAuthEmulator} from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAHL3a8NgIR31S97JHsAu2fG_1EMy2jb7o",
@@ -18,14 +18,14 @@ const firebaseConfig = {
     appId: "1:229925355235:web:4fe2a98d0769b1a034cfd6"
 };
 
-const firebase = initializeApp(firebaseConfig);
-const db = getFirestore(firebase);
-const functions = getFunctions(firebase);
-const auth = getAuth(firebase);
+initializeApp(firebaseConfig);
+// const db = getFirestore(firebase);
+// const functions = getFunctions(firebase);
+// const auth = getAuth(firebase);
 
-connectFirestoreEmulator(db, 'localhost', 8080);
-connectFunctionsEmulator(functions, 'localhost', 5001);
-connectAuthEmulator(auth, 'http://localhost:9099');
+// connectFirestoreEmulator(db, 'localhost', 8080);
+// connectFunctionsEmulator(functions, 'localhost', 5001);
+// connectAuthEmulator(auth, 'http://localhost:9099');
 
 const Order = lazy(() => import('./components/Order/Order'));
 const Overview = lazy(() => import('./components/overview/Overview'));
