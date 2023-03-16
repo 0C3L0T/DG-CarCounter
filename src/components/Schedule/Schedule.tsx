63df1888830b841a9304e84b90d72217ed706305
Jake Jongejans <jake.jongejans@enreach.com>
2023-03-16 21:03:55 +0100

    chore: Fixes schedule layout.

--- src/components/Schedule/Schedule.tsx
+++ src/components/Schedule/Schedule.tsx
@@ -1,41 +1,43 @@
 import type { Component } from "solid-js";
-import {createSignal, lazy, Show} from "solid-js";
-import {onAuthStateChanged, getAuth, User} from "firebase/auth";
+import { createSignal, lazy, Show } from "solid-js";
+import { onAuthStateChanged, getAuth, User } from "firebase/auth";
 
-const ScheduleDay = lazy(() => import('./ScheduleDay'));
-const Login = lazy(() => import('../Login/Login'));
+const ScheduleDay = lazy(() => import("./ScheduleDay"));
+const Login = lazy(() => import("../Login/Login"));
 
 import "./schedule.scss";
 
 const auth = getAuth();
 
 const Schedule: Component = () => {
-    const [user, setUser] = createSignal<User|null>(null);
+  const [user, setUser] = createSignal<User | null>(null);
 
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-            setUser(user);
-        }
-    });
-    const today = new Date();
-    const tomorrow = new Date();
-    const dayAfterTomorrow = new Date();
-    const dayAfterDayAfterTomorrow = new Date();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setUser(user);
+    }
+  });
+  const today = new Date();
+  const tomorrow = new Date();
+  const dayAfterTomorrow = new Date();
+  const dayAfterDayAfterTomorrow = new Date();
 
-    tomorrow.setDate(today.getDate() + 1);
-    dayAfterTomorrow.setDate(today.getDate() + 2);
-    dayAfterDayAfterTomorrow.setDate(today.getDate() + 3);
+  tomorrow.setDate(today.getDate() + 1);
+  dayAfterTomorrow.setDate(today.getDate() + 2);
+  dayAfterDayAfterTomorrow.setDate(today.getDate() + 3);
 
-    return (
-        <Show when={user()} fallback={<Login/>} keyed>
-             <div class="schedule">
-                 <ScheduleDay date={today}/>
-                 <ScheduleDay date={tomorrow}/>
-                 <ScheduleDay date={dayAfterTomorrow}/>
-                 <ScheduleDay date={dayAfterDayAfterTomorrow}/>
-             </div>
-        </Show>
-    );
+  return (
+    <Show when={user()} fallback={<Login />} keyed>
+      <div class="schedule">
+        <div class="schedule__days">
+          <ScheduleDay date={today} />
+          <ScheduleDay date={tomorrow} />
+          <ScheduleDay date={dayAfterTomorrow} />
+          <ScheduleDay date={dayAfterDayAfterTomorrow} />
+        </div>
+      </div>
+    </Show>
+  );
 };
 
 export default Schedule;
